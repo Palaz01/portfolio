b6c5182ba83e156963c6fa33d0074182f6ebc327
Palaz <zoltan.palatinus@inteliart.hu>
2018-10-18 15:53:46 +0200

    skill circles

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,18 +2,27 @@
 	<transition enter-active-class="animated zoomIn">
 		<main id="skills">
 			<h1 class="lg-heading">
-			Contact
-			<span class="text-secondary">Me</span>
+			My
+			<span class="text-secondary">Skills</span>
 			</h1>
 			<h2 class="sm-heading">
-			This is how you can reach me...
+			I have experience in these technologies
 			</h2>
 			<div class="skills-wrap">
-				<div>
-				</div>
-				<div>
-				</div>
-				<div>
+				<div  v-for="(data, index) in skills" :key='index'>
+					<vue-circle
+						:progress='data.progress'
+						:size="180"
+						:reverse="false"
+						line-cap="round"
+						:fill="fill"
+						empty-fill="rgba(255, 255, 255, .1)"
+						:animation-start-value="0.0"
+						insert-mode="append"
+						:thickness="12"
+						:show-percent="false">
+						<h2>{{data.skillName}}</h2>
+					</vue-circle>
 				</div>
 			</div>
 		</main>
@@ -21,8 +30,35 @@
 </template>
 
 <script>
-    export default {
-    }
+	import VueCircle from 'vue2-circle-progress/src/index.vue'
+	export default {
+	components: {
+		VueCircle
+	},
+	data(){
+      return{
+        fill : { color: '#64ffda' },
+		skills: [
+			{
+				progress: 80,
+				skillName: 'Html5'
+			},
+			{
+				progress: 80,
+				skillName: 'CSS/Scss'
+			},
+			{
+				progress: 30,
+				skillName: 'Javascript'
+			},
+			{
+				progress: 70,
+				skillName: 'Wordpress'
+			}			
+		]
+      }
+	}
+}
 </script>
 
 <style scoped>
