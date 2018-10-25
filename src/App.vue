99cccb67b64d456f4537e9d5c77222104f4f06fe
Palaz <zoltan.palatinus@inteliart.hu>
2018-10-25 14:15:22 +0200

    change nav (WIP)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
 	<div id="app">
-		<nav class="main-navigation">
-			<router-link to="/">Home</router-link>
-			<router-link to="/about">About</router-link>
-			<router-link to="/skills">Skills</router-link>
-			<router-link to="/work">Work</router-link>
-			<router-link to="/contact">Contact</router-link>
-		</nav>
-
+		<header>
+			<button class="menu-btn" v-bind:class="{close: showNav}" v-on:click="openNav()">
+				<div class="btn-line"></div>
+				<div class="btn-line"></div>
+				<div class="btn-line"></div>
+			</button>			
+			<nav  class="menu">
+				<transition enter-active-class="animated slideInDown">
+					<div v-if="showNav" class="menu-branding">
+						<div class="portrait"></div>
+					</div>
+				</transition>
+				<transition enter-active-class="animated slideInUp">
+					<div v-if="showNav" class="menu-nav">
+						<router-link class="nav-link" to="/" v-on:click.native="openNav()">Home</router-link>
+						<router-link class="nav-link" to="/about" v-on:click.native="openNav()">About</router-link>
+						<router-link class="nav-link" to="/skills" v-on:click.native="openNav()">Skills</router-link>
+						<router-link class="nav-link" to="/work" v-on:click.native="openNav()">Work</router-link>
+						<router-link class="nav-link" to="/contact" v-on:click.native="openNav()">Contact</router-link>
+					</div>
+				</transition>
+			</nav>
+		</header>
 		<router-view />
 	</div>
 </template>
@@ -19,6 +34,20 @@ export default {
 	name: 'app',
 	components: {
 		Home
+	},
+	data() {
+		return {
+			showNav: false
+		}
+	},
+	methods: {
+		openNav: function() {
+			if(!this.showNav) {
+				this.showNav = true;
+			} else {
+				this.showNav = false;
+			}
+		}
 	}
 }
 </script>
